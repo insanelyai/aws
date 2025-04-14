0f149a1e19452ff5e694d595ee2089397638f4d5
insanelyai <hasansayyed.personal@gmail.com>
2025-04-14 21:11:17 +0530

    feat: configure CORS headers and remove TypeScript config
    - Added CORS headers to the Next.js configuration for handling cross-origin requests.
    - Set allowed methods and headers for API requests based on the environment.
    - Removed the TypeScript configuration file as it is no longer needed.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,33 @@ const nextConfig = {
   images: {
     domains: ["awsclouded.s3.us-east-1.amazonaws.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              process.env.NODE_ENV === "development"
+                ? "http://18.215.226.190:3000"
+                : "http://18.215.226.190:3000", // Replace with your actual domain
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+  experimental: {
+    allowedDevOrigins: ["18.215.226.190"],
+  },
 };
 
 module.exports = nextConfig;
